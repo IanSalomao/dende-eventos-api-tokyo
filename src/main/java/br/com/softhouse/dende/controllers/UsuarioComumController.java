742cf70b542f7e8b794e9a4c41c3617d5487828d
valeriasoars <valerisoaressantos2@gmail.com>
2026-02-19 15:54:45 -0300

    a rota de comprar ingresso que esqueci de adiconar.

--- src/main/java/br/com/softhouse/dende/controllers/UsuarioComumController.java
+++ src/main/java/br/com/softhouse/dende/controllers/UsuarioComumController.java
@@ -1,14 +1,12 @@
 package br.com.softhouse.dende.controllers;
 
 import br.com.dende.softhouse.annotations.Controller;
-import br.com.dende.softhouse.annotations.request.GetMapping;
-import br.com.dende.softhouse.annotations.request.PathVariable;
-import br.com.dende.softhouse.annotations.request.PutMapping;
-import br.com.dende.softhouse.annotations.request.RequestMapping;
+import br.com.dende.softhouse.annotations.request.*;
 import br.com.dende.softhouse.process.route.ResponseEntity;
 import br.com.softhouse.dende.model.Evento;
 import br.com.softhouse.dende.model.Ingresso;
 import br.com.softhouse.dende.model.UsuarioComum;
+import br.com.softhouse.dende.model.dto.CompraIngressoDTO;
 import br.com.softhouse.dende.model.dto.IngressoDTO;import br.com.softhouse.dende.repositories.Repositorio;
 
 import java.util.List;
@@ -21,6 +19,10 @@ public class UsuarioComumController {
     public UsuarioComumController(){
         this.repositorio = Repositorio.getInstance();
     }
+
+
+
+
 
     @GetMapping(path = "/{usuarioId}/ingressos")
     public ResponseEntity<?> listarIngressos(@PathVariable(parameter = "usuarioId") Long usuarioId) {
@@ -75,4 +77,41 @@ public class UsuarioComumController {
         }
     }
 
+
+    @PostMapping(path = "/{usuarioId}/eventos/{eventoId}/ingresso")
+    public ResponseEntity<?> comprarIngresso(
+            @PathVariable(parameter = "usuarioId") Long usuarioId,
+            @PathVariable(parameter = "eventoId") Long eventoId) {
+        try {
+            UsuarioComum usuario = (UsuarioComum) repositorio.buscarUsuarioPorId(usuarioId);
+            Evento evento = repositorio.buscarEventoPorId(eventoId);
+
+            CompraIngressoDTO compra = usuario.comprarIngresso(evento);
+
+            compra.ingressos().forEach(repositorio::salvarIngresso);
+
+            List<IngressoDTO> ingressosDTO = compra.ingressos()
+                    .stream()
+                    .map(i -> new IngressoDTO(
+                            i.getId(),
+                            i.getEvento().getNome(),
+                            i.getEvento().getDataHoraInicio(),
+                            i.getValorPago(),
+                            i.getStatus().name(),
+                            i.getDataCompra()
+                    ))
+                    .toList();
+
+            return ResponseEntity.ok(
+                    new Object() {
+                        public final double valorTotal = compra.valorTotal();
+                        public final List<IngressoDTO> ingressos = ingressosDTO;
+                    }
+            );
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(400, e.getMessage());
+        } catch (ClassCastException e) {
+            return ResponseEntity.status(400, "Usuário informado não é um usuário comum.");
+        }
+    }
 }
